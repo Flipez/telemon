4a02b8cced16869bb1e1cde78727e3b0b7d4ed08
Flipez <github@flipez.net>
2016-01-14 17:56:20 +0000

    should work I guess...
    Signed-off-by: Flipez <github@flipez.net>

--- lib/telemon.rb
+++ lib/telemon.rb
@@ -6,19 +6,15 @@ module Telemon
 
   extend GLI::App
 
-  CONFIG = File.join(ENV['HOME'], '.telemon.yml')
+  CONFIG = YAML.load_file(File.join(ENV['HOME'], '.telemon.yml'))
 
   program_desc 'Push you message to Telegram'
-  pre do |global_options, options, args|
-    CONFIG.merge!(YAML.load_file(global_options[:config]))
-  end
 
   desc 'Push to Telegram'
   command :push do |c|
     c.flag [:chat_id], default_value: CONFIG['chat_id']
     c.flag [:token], default_value: CONFIG['token']
     c.action do |global_options,options,args|
-
       message = args[0]
       Telegram::Bot::Client.run(options[:token]) do |bot|
         bot.api.send_message(chat_id: options[:chat_id], text: message.to_s)
