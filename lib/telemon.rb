117c2487b19abbd9fb241e612e51d0147d4fb3ca
Flipez <github@flipez.net>
2016-01-14 17:45:44 +0000

    fix config handling, bump version
    Signed-off-by: Flipez <github@flipez.net>

--- lib/telemon.rb
+++ lib/telemon.rb
@@ -6,17 +6,22 @@ module Telemon
 
   extend GLI::App
 
-  CONFIG = YAML.load_file('/home/robert/git/telemond/config.yml')
-  
+  CONFIG = File.join(ENV['HOME'], '.telemon.yml')
+
+  program_desc 'Push you message to Telegram'
+  pre do |global_options, options, args|
+    CONFIG.merge!(YAML.load_file(global_options[:config]))
+  end
+
+  desc 'Push to Telegram'
   command :push do |c|
+    c.flag [:chat_id], default_value: CONFIG['chat_id']
+    c.flag [:token], default_value: CONFIG['token']
     c.action do |global_options,options,args|
-      p args
+
       message = args[0]
-  
-      token = CONFIG['token']
-      chat_id = CONFIG['chat_id']
-      Telegram::Bot::Client.run(token) do |bot|
-        bot.api.send_message(chat_id: chat_id, text: message)
+      Telegram::Bot::Client.run(options[:token]) do |bot|
+        bot.api.send_message(chat_id: options[:chat_id], text: message.to_s)
       end
     end
   end
